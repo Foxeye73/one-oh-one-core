6c3876ac0d823c781fa9f9d4d6ad2ce8e7424fbe
Andrew Simpson <andrew@101collective.com>
2026-03-12 16:06:43 +0200

    0.1.4 reveerted crud to previous version, to deal correct data types

--- one_oh_one_core/controllers/async_crud_controller.py
+++ one_oh_one_core/controllers/async_crud_controller.py
@@ -9,9 +9,8 @@ CreateSchemaType = TypeVar("CreateSchemaType")
 UpdateSchemaType = TypeVar("UpdateSchemaType")
 
 
-class AsyncCRUDController(
-    Generic[ModelType, SchemaType, CreateSchemaType, UpdateSchemaType]
-):
+class AsyncCRUDController(Generic[ModelType, SchemaType, CreateSchemaType, UpdateSchemaType]):
+
     model: Type[ModelType]
     schema: Type[SchemaType]
 
@@ -21,24 +20,29 @@ class AsyncCRUDController(
     @route.get("/", response=List[SchemaType])
     @paginate
     async def list(self):
-        return self.get_queryset()
+        qs = self.get_queryset()
+        return [self.schema.from_orm(obj) async for obj in qs]
 
     @route.get("/{obj_id}", response=SchemaType)
     async def retrieve(self, obj_id: int):
         obj = await self.get_queryset().aget(id=obj_id)
-        return obj
+        return self.schema.from_orm(obj)
 
     @route.post("/", response=SchemaType)
     async def create(self, payload: CreateSchemaType):
         obj = await self.model.objects.acreate(**payload.model_dump())
-        return obj
+        return self.schema.from_orm(obj)
 
     @route.patch("/{obj_id}", response=SchemaType)
     async def update(self, obj_id: int, payload: UpdateSchemaType):
         obj = await self.get_queryset().aget(id=obj_id)
-
         for k, v in payload.model_dump(exclude_unset=True).items():
             setattr(obj, k, v)
-
         await obj.asave()
-        return obj
+        return self.schema.from_orm(obj)
+
+    # @route.delete("/{obj_id}")
+    # async def delete(self, obj_id: int):
+    #     # obj = await self.get_queryset().aget(id=obj_id)
+    #     # await obj.adelete()
+    #     return {"success": True}
