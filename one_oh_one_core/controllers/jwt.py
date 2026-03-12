8165b2f6bb443e06f43afff996536e28eb364dc1
Andrew Simpson <andrew@101collective.com>
2026-03-12 14:38:37 +0200

    0.1.1 fiix to asyn crud

--- one_oh_one_core/controllers/jwt.py
+++ one_oh_one_core/controllers/jwt.py
@@ -4,14 +4,9 @@ from django.contrib.auth.models import AbstractUser
 from ninja import Schema
 from ninja_extra import api_controller, route
 from ninja_extra.permissions import AllowAny
-from ninja_jwt.controller import NinjaJWTDefaultController
+from ninja_jwt.controller import NinjaJWTController
 from ninja_jwt.schema import TokenObtainPairInputSchema
 from ninja_jwt.tokens import RefreshToken
-
-
-"""
-This module contains the controller and related schemas for the token generation
-"""
 
 
 class UserSchema(Schema):
@@ -28,6 +23,7 @@ class MyTokenObtainPairOutSchema(Schema):
 
 
 class MyTokenObtainPairSchema(TokenObtainPairInputSchema):
+
     def output_schema(self):
         out_dict = self.get_response_schema_init_kwargs()
         out_dict.update(user=UserSchema.from_orm(self._user))
@@ -36,18 +32,23 @@ class MyTokenObtainPairSchema(TokenObtainPairInputSchema):
     @classmethod
     def get_token(cls, user: AbstractUser) -> Dict:
         values = {}
+
         refresh = RefreshToken.for_user(user)
         refresh = cast(RefreshToken, refresh)
+
         values["refresh"] = str(refresh)
         values["access"] = str(refresh.access_token)
-        values["duration"] = refresh.access_token.payload['exp'] - refresh.access_token.payload['iat']
+        values["duration"] = (
+            refresh.access_token.payload["exp"]
+            - refresh.access_token.payload["iat"]
+        )
+
         return values
 
 
-@api_controller('/token', permissions=[AllowAny], tags=["Auth"], auth=None)
-class MyTokenObtainPairController(NinjaJWTDefaultController):
-    @route.post(
-        "/pair", response=MyTokenObtainPairOutSchema, url_name="token_obtain_pair"
-    )
+@api_controller("/token", permissions=[AllowAny], tags=["Auth"])
+class MyTokenObtainPairController(NinjaJWTController):
+
+    @route.post("/pair", response=MyTokenObtainPairOutSchema)
     def obtain_token(self, user_token: MyTokenObtainPairSchema):
         return user_token.output_schema()
